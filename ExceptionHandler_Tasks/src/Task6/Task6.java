bd3833c43af7bdd7480443633bb8a29fb404dbd6
SSalahAyman <salahayman488@gmail.com>
2026-02-02 11:19:58 +0200

    update on the solution of task 6

--- ExceptionHandler_Tasks/src/Task6/Task6.java
+++ ExceptionHandler_Tasks/src/Task6/Task6.java
@@ -7,13 +7,15 @@ public class Task6 {
         int secondNumber=0;
         try{
             System.out.println(in.toUpperCase());  // >>> this line throw Null pointerException
-            int result;
-            result=firstNumber/secondNumber;  // >>> this line throw ArithmeticException
-            System.out.println(result);
         }
         // So, we handle the Null pointer Exception with this catch with print Statement "Error" instead of crash the program
         catch (NullPointerException e){
             System.out.println("Error Null reference used");
+        }
+        try {
+            int result;
+            result=firstNumber/secondNumber;  // >>> this line throw ArithmeticException
+            System.out.println(result);
         }
         // So, we handle the ArithmeticException with this catch with print Statement "Error" instead of crash the program
         catch (ArithmeticException e){
