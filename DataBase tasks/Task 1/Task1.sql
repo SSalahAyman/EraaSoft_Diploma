58cfcdef004c41071ae68df61a1704d5d58ac2d2
SSalahAyman <salahayman488@gmail.com>
2026-03-17 22:14:57 +0200

    finished task 1

---
+++ DataBase tasks/Task 1/Task1.sql
@@ -0,0 +1,13 @@
+/*
+ * create table Manager contain (id, name , age , birth_date , address)
+ */
+CREATE TABLE Manager (
+  id                NUMBER(3),
+  name              VARCHAR2(100),
+  age               NUMBER(3),
+  birth_date        DATE,
+  address           VARCHAR2(255),
+);
+
+/**
+ * 
